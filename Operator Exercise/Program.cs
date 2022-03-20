06ba6312d0e9b4c77dd6aa6153d60c34ee27019b
Thomas Marble <tmarble3@gmail.com>
2022-03-20 09:22:33 -0600

    Add project files.

---
+++ Operator Exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Operator_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} and the remainder is {mod}");
+
+            var r = 20;
+            var pi = Math.PI;
+
+            Console.WriteLine("What is the radius?");
+            var radius = double.Parse(Console.ReadLine());
+            var areaOfCircle = CalculateArea(radius);
+
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {areaOfCircle}.");
+
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+
+
+    }
+}
